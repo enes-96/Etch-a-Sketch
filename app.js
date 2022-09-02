e1c47b9dce3d5c3afd820cbde06177ae4f78eb15
Enes <seyit7enes@gmail.com>
2022-09-02 17:40:23 +0200

    commenting some javascript

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const container = document.querySelector(".container");
 const rangeValue = document.querySelector(".grid-size");
 const gridSize = document.querySelector("h2");
 rangeValue.onchange = (e) => {
+  //clear the grid for a new one
   clearGrid();
+  ///generating new grid
   gridGenerator(e.target.value);
 };
 
@@ -23,15 +25,19 @@ function gridGenerator(size) {
       gridElement.style.backgroundColor = "red";
     });
   }
+  //displaying the grid size
   updateGridSize(size);
 }
 
+//clearing the grid
 function clearGrid() {
   container.innerHTML = "";
 }
 
+//updating the grid size display
 function updateGridSize(size) {
   gridSize.innerHTML = `${size} x ${size}`;
 }
 
+calling the default grid
 gridGenerator(16);
