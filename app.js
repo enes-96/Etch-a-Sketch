65ff826d4596d7be463f26aa80719638ba1afe45
Enes <seyit7enes@gmail.com>
2022-09-02 17:27:37 +0200

    creating basic sketch by myself

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 "use strict";
 //ich könnte so machen, wenn movein oder mousein ich weiss nocht nicht style background black
+const container = document.querySelector(".container");
+const child = document.querySelector(".child");
+
+const childColor = function () {};
+
+child.addEventListener("click", function () {});
